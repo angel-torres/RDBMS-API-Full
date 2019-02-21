e66ee28ff4cb25d6d37c6a8071770b6fd929b444
Angel Torres <alas4224@gmail.com>
2019-02-20 18:22:28 -0600

    add students table

--- migrations/20190220172924_bootstrap.js
+++ migrations/20190220172924_bootstrap.js
@@ -12,5 +12,5 @@ exports.up = function(knex, Promise) {
 
 // how to undo database changes
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("cohorts");
+    return knex.schema.dropTableIfExists("cohorts");
 };
